61ee7ab88dea5e016a8261bde966347dabda6f1a
Charles Marcoin <charles.marcoin@gmail.com>
2017-05-22 15:21:07 +0200

    installed devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Absolute URLs for Devise Gem
+
+  config.action_mailer.default_url_options = { host: 'https://mes-patissiers-cmarcoin.herokuapp.com/' }
+
+
 end
