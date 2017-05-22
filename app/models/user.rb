02be6be47dd4ea2ac7b6d2929cf7a19650ac279d
Patrick Berczelly <patrick.berczelly@grenoble-em.com>
2017-05-22 16:26:52 +0200

    validations add to user and all the input add in signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates_format_of :phone_number, with: /\A(0|\+33|0033)[1-9]([-. ]?[0-9]{2}){4}$\Z/
 end
