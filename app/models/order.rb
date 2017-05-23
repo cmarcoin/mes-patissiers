67dd36dd24ee7922a408226d32c6daadff8df034
Charles Marcoin <charles.marcoin@gmail.com>
2017-05-23 09:09:20 +0200

    added a validation on the status field for the order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :pastry
+  validates :status, presence: true, inclusion: { in: ["En attente de validation", "validée"],
+  message: "%{value} n'est pas un statut valide pour une commande" }
 end
