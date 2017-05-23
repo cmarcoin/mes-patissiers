47cca356a84e40a6d93da847a3dad9a765b11118
kara <kdiaby.pro@gmail.com>
2017-05-23 09:29:49 +0200

    Modif routes.rb with order inclusion

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  # Authentication management
   devise_for :users
+
+  # Users management (with orders)
+  resources :users, only: [:show] do
+    resources :orders, only: [:new, :create, :show, :destroy]
+  end
+
+  # Custom pages
   root to: 'pages#home'
   get 'pastries/search' => 'pastries#search' # recherche dans la home page
-  resources :pastries
-  get 'users/:id' => 'users#show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :pastrieshow
 end
