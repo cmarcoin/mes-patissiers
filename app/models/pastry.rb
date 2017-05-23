2960efd4986663681f09e06b600b1a82ffa1fb45
Charles Marcoin <charles.marcoin@gmail.com>
2017-05-23 17:07:45 +0200

    Update pastry.rb

--- app/models/pastry.rb
+++ app/models/pastry.rb
@@ -2,11 +2,4 @@ class Pastry < ApplicationRecord
   belongs_to :baker, class_name: "User", foreign_key: :baker_id
   has_many :orders
   mount_uploader :photo, PhotoUploader
-
-  def self.search(search)
-
-    User.near("%#{search}%", 5)
-
-  end
-
 end
