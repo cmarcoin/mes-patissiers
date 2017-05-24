8cd19e4679a4aaaf0a8f7b927d6fa36eea0d7fb4
kara <kdiaby.pro@gmail.com>
2017-05-24 12:17:40 +0200

    add datepicker to order form in pastries/show view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,4 +3,10 @@
 //= require bootstrap-sprockets
 //= require underscore
 //= require gmaps/google
+//= require bootstrap-datepicker
 //= require_tree .
+
+$('#order_delivery_date').datepicker({
+  format: 'dd/mm/yyyy',
+  language: 'fr'
+});
